aa12e787736b67b455ebc9173307edb56e675072
ryan.wang <ryan.wang@vipshop.com>
2016-08-07 00:53:07 +0800

    修改自动拉取分支

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -18,7 +18,7 @@ class WeixinController extends Controller
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 IMS！";
+            return "欢迎关注 IMS1101！";
         });
 
         Log::info('return response.');
