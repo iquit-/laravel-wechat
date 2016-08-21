be217d2167574a5612db765226c0387afdbba064
ryan.wang <ryan.wang@vipshop.com>
2016-08-21 20:59:23 +0800

    参数解析

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -18,11 +18,60 @@ class WeixinController extends Controller
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 IMS1101-house_price_analysis！";
+            switch ($message->Event) {
+				case 'subscribe':
+					return self::getSubscribeResp();
+				default:
+					break;
+			}
+			switch ($message->MsgType) {
+				case 'text':
+					return self::getTextResp();
+				case 'image':
+					return self::getImageResp();
+				case 'voice':
+					return self::getVoiceResp($message);
+				case 'video':
+					return self::getVideoResp();
+				case 'location':
+					return self::getLocationResp();
+				case 'link':
+					return self::getLinkResp();
+				default:
+					break;
+			}
         });
 
         Log::info('return response.');
 
         return $wechat->server->serve();
     }
+	
+	public function getSubscribeResp() {
+		return "欢迎关注！我们将会持续推送房价信息与变化趋势！嘻嘻！";
+	}
+
+	public function getTextResp() {
+		return "这是文本！";
+	}
+	
+	public function getImageResp() {
+		return "这是图片！";
+	}
+	
+	public function getVoiceResp($message) {
+		return "你说的是".$message->MsgType."吗？".$message->Recongnition;
+	}
+	
+	public function getVideoResp() {
+		return "这是视频！";
+	}
+	
+	public function getLocationResp() {
+		return "这是坐标！";
+	}
+	
+	public function getLinkResp() {
+		return "这是链接！";
+	}
 }
