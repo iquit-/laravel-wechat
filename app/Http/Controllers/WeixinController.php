b0178ed360fbfebb219bfa33392f64d8bbc28e14
ryan.wang <ryan.wang@vipshop.com>
2016-08-30 00:33:47 +0800

    调用百度api

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -78,6 +78,8 @@ class WeixinController extends Controller
 	}
 	
 	public function getEventLocationResp($message) {
-		return "地理位置纬度：" . $message->Latitude . "，地理位置经度：" . $message->Longitude . "，地理位置精度：" . $message->Precision;
+		$url = "http://api.map.baidu.com/geocoder/v2/?ak=16a13b37a67c9102c3d825b5dc1674f9&location=".$message->Latitude.",".$message->Longitude."&output=json&pois=1"
+		$html = file_get_contents($url);
+		return "地理位置纬度：" . $message->Latitude . "，地理位置经度：" . $message->Longitude . "，地理位置精度：" . $message->Precision . $html;
 	}
 }
