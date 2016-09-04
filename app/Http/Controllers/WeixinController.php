db8f42f30eef37a1047f8c0feb93d482fc5e8525
ryan.wang <ryan.wang@vipshop.com>
2016-09-04 20:24:35 +0800

    代码优化

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -81,7 +81,6 @@ class WeixinController extends Controller
 	
 	public function getEventLocationResp($message) {
 		$url = self::$baiduMapApiUrl . $message->Latitude . "," . $message->Longitude;
-		return $url;
 		$html = file_get_contents($url);
 		$obj=json_decode($html);
 		$city =  $obj->result->addressComponent->city;
