59e1bd0b760493ff1aedd14d908730dd23429d10
ryan.wang <ryan.wang@vipshop.com>
2016-11-13 19:48:34 +0800

    新增游戏房间控制层

---
+++ app/Http/Controllers/GameRoomController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use EasyWeChat\Foundation\Application;
+
+class GameRoomController extends Controller
+{
+		$config = [
+			'oauth' => [
+				'scopes'   => ['snsapi_userinfo'],
+				'callback' => '/oauth_callback',
+			],
+		];
+		
+		$app = new Application($config);
+		
+		$oauth = $app->oauth;
+		
+		public function serve()
+		{
+			if (empty($_SESSION['wechat_user'])) {
+				$_SESSION['target_url'] = 'user/profile';
+				return $oauth->redirect();
+}
+		}
+}
