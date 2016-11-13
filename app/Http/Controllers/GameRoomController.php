9f92d8ef770d68af2beb92cc8e1904fb5e396235
ryan.wang <ryan.wang@vipshop.com>
2016-11-13 20:10:39 +0800

    代码优化

--- app/Http/Controllers/GameRoomController.php
+++ app/Http/Controllers/GameRoomController.php
@@ -27,5 +27,23 @@ class GameRoomController extends Controller
 			$_SESSION['target_url'] = 'user/profile';
 			return $oauth->redirect();
 		}
+		$user = $_SESSION['wechat_user'];
+		return $user;
 	}
+	
+	public function oauthCallback() {
+		$config = [
+			// ...
+		];
+		
+		$app = new Application($config);
+		
+		$oauth = $app->oauth;
+		// 获取 OAuth 授权结果用户信息
+		$user = $oauth->user();
+		$_SESSION['wechat_user'] = $user->toArray();
+		$targetUrl = empty($_SESSION['target_url']) ? '/' : $_SESSION['target_url'];
+		return response()->json(['name' => 'oauthCallback', 'state' => 'CA']);
+	}
+	
 }
