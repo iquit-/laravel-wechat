43cb12be4cf52999f8ba2a54ef805805ae924990
ryan.wang <ryan.wang@vipshop.com>
2016-11-13 19:59:51 +0800

    代码优化

--- app/Http/Controllers/GameRoomController.php
+++ app/Http/Controllers/GameRoomController.php
@@ -10,19 +10,19 @@ use EasyWeChat\Foundation\Application;
 
 class GameRoomController extends Controller
 {
-	public static $config = [
-		'oauth' => [
-			'scopes'   => ['snsapi_userinfo'],
-			'callback' => '/oauth_callback',
-		],
-	];
-		
-	$app = new Application($config);
-		
-	$oauth = $app->oauth;
-		
 	public function serve()
 	{
+		$config = [
+			'oauth' => [
+				'scopes'   => ['snsapi_userinfo'],
+				'callback' => '/oauth_callback',
+			],
+		];
+		
+		$app = new Application($config);
+		
+		$oauth = $app->oauth;
+		
 		if (empty($_SESSION['wechat_user'])) {
 			$_SESSION['target_url'] = 'user/profile';
 			return $oauth->redirect();
